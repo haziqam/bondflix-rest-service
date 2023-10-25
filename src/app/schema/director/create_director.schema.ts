cd09a91e4cb1ce356d90023cac90d31954724ace
Haziq <haziqabiyyu@gmail.com>
2023-10-25 14:39:29 +0700

    refactor: remove unused attributes in zod schema

--- src/app/schema/director/create_director.schema.ts
+++ src/app/schema/director/create_director.schema.ts
@@ -1,12 +1,5 @@
-// CREATE TABLE IF NOT EXISTS director (
-//     director_id SERIAL PRIMARY KEY,
-//     first_name VARCHAR(255) NOT NULL,
-//     last_name VARCHAR(50)
-// );
-
 import {z} from 'zod'
 
 export const createDirectorSchema = z.object({
-    first_name: z.string().min(1, "First name may not be empty"),
-    last_name: z.string().min(1, "Last name may not be empty"),
+    name: z.string().min(1, "Director name may not be empty"),
 })
--- src/app/schema/director/update_director.schema.ts
+++ src/app/schema/director/update_director.schema.ts
@@ -1,6 +1,5 @@
 import {z} from 'zod'
 
 export const updateDirectorSchema = z.object({
-    first_name: z.string().min(1, "First name may not be empty").optional(),
-    last_name: z.string().min(1, "Last name may not be empty").optional(),
+    name: z.string().min(1, "Directors name may not be empty"),
 })

0e465930e6ffa93c47301f459d04ed93f98ae811
Haziq <haziqabiyyu@gmail.com>
2023-11-04 19:47:14 +0700

    feat: more methods in soap.client

--- src/app/adapters/soap/soap.client.ts
+++ src/app/adapters/soap/soap.client.ts
@@ -1,4 +1,4 @@
-import {SoapUtils} from "../../utils/soap.utils";
+import { SoapUtils } from "../../utils/soap.utils";
 
 export class SoapClient {
     private static instance: SoapClient;
@@ -11,7 +11,7 @@ export class SoapClient {
     public static getInstance(url?: string): SoapClient {
         if (!SoapClient.instance) {
             const soapServiceURL = process.env.SOAP_SERVICE_URL;
-            if (url == null || url == '' && soapServiceURL != undefined) {
+            if (url == null || (url == "" && soapServiceURL != undefined)) {
                 // @ts-ignore
                 SoapClient.instance = new SoapClient(soapServiceURL);
             } else {
@@ -24,17 +24,55 @@ export class SoapClient {
     public async getAdd(a: number, b: number) {
         const args = {
             arg1: a,
-            arg2: b
+            arg2: b,
         };
         return await this.soapUtils.call("add", args);
     }
 
-    public async addCreatorSubscriberRelationship(creatorId: number, subscriberId: number) {
-        const args= {
-            creatorId: creatorId,
-            subscriberId: subscriberId
-        }
+    public async addCreatorSubscriberRelationship(
+        creatorId: number,
+        subscriberId: number
+    ) {
+        const args = {
+            creatorId,
+            subscriberId,
+        };
 
-        return await this.soapUtils.call("addCreatorSubscriberRelationship", args)
+        return await this.soapUtils.call(
+            "addCreatorSubscriberRelationship",
+            args
+        );
+    }
+
+    public async isUserSubscribedToCreator(userId: number, creatorId: number) {
+        const args = {
+            userId,
+            creatorId,
+        };
+
+        return await this.soapUtils.call("isUserSubscribedToCreator", args);
+    }
+
+    public async getSubscribedUsers() {
+        const args = {};
+        return await this.soapUtils.call("getSubscribedUsers", args);
+    }
+
+    public async updateSubscriptionByCreatorId(
+        creatorId: number,
+        subscriptionStatus: boolean
+    ) {
+        const args = { creatorId, subscriptionStatus };
+        return await this.soapUtils.call("updateSubscriptionByCreatorId", args);
+    }
+
+    public async getSubscribersByCreatorId(creatorId: number) {
+        const args = { creatorId };
+        return await this.soapUtils.call("getSubscribersByCreatorId", args);
+    }
+
+    public async getCreatorsBySubscriberId(subscriberId: number) {
+        const args = { subscriberId };
+        return await this.soapUtils.call("getCreatorsBySubscriberId", args);
     }
 }

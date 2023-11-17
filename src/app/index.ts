cbc2730b43832af04c5e21324fa343d7541c4520
Nicholas Liem <13521135@std.stei.itb.ac.id>
2023-11-17 14:39:33 +0700

    feat: add cors

--- src/app/index.ts
+++ src/app/index.ts
@@ -11,9 +11,19 @@ dotEnv.config();
 
 app.use(express.json());
 app.use(cookieParser());
+const allowedOrigins = ["http://localhost:5173", "http://localhost:8081"];
+
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: function (origin, callback) {
+            if (!origin) return callback(null, true);
+
+            if (allowedOrigins.indexOf(origin) === -1) {
+                var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+                return callback(new Error(msg), false);
+            }
+            return callback(null, true);
+        },
         methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
         allowedHeaders: ["Content-Type", "Authorization"],
         credentials: true,
